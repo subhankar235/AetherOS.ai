bcc311fa4199b48775c98f14e72bc59d7303eda9
subhankar235 <nathsubhankar57@gmail.com>
2026-07-17 06:35:20 +0530

    added-gmail:integration

---
+++ apps/api/core/celery_app.py
@@ -0,0 +1,16 @@
+from celery import Celery
+from core.config import settings
+
+celery_app = Celery(
+    "aetheros_tasks",
+    broker=settings.CELERY_BROKER_URL,
+    backend=settings.CELERY_RESULT_BACKEND
+)
+
+celery_app.conf.update(
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+    timezone="UTC",
+    enable_utc=True,
+)
